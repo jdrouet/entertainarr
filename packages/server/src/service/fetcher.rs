4f8409b19c22573f27116555193d9a23c0576927
Jérémie Drouet <jeremie.drouet@gmail.com>
2025-05-11 14:26:04 +0200

    feat: configure cache path
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- packages/server/src/service/fetcher.rs
+++ packages/server/src/service/fetcher.rs
@@ -1,17 +1,38 @@
-use std::sync::Arc;
+use std::{path::PathBuf, sync::Arc};
 
 use http_cache_reqwest::{CACacheManager, Cache, CacheMode, HttpCache, HttpCacheOptions};
 use reqwest_middleware::ClientWithMiddleware;
 
-#[derive(Debug, Default, serde::Deserialize)]
-pub struct Config {}
+#[derive(Debug, serde::Deserialize)]
+pub struct Config {
+    #[serde(default = "Config::default_path")]
+    path: PathBuf,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            path: Self::default_path(),
+        }
+    }
+}
 
 impl Config {
+    fn default_path() -> PathBuf {
+        PathBuf::from(".cache")
+    }
+
+    fn manager(&self) -> CACacheManager {
+        CACacheManager {
+            path: self.path.clone(),
+        }
+    }
+
     pub fn build(&self) -> std::io::Result<Fetcher> {
         let client = reqwest_middleware::ClientBuilder::new(reqwest::Client::new())
             .with(Cache(HttpCache {
                 mode: CacheMode::ForceCache,
-                manager: CACacheManager::default(),
+                manager: self.manager(),
                 options: HttpCacheOptions::default(),
             }))
             .build();
